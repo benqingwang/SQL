34e366241168291b1c446a00d6e9b302120c4308
benqingwang <158376214+benqingwang@users.noreply.github.com>
2024-05-12 09:34:19 -0400

    Update 01_basics.sql

--- 01_basics.sql
+++ 01_basics.sql
@@ -54,48 +54,4 @@ GROUP BY a
       WHEN id%2=0 THEN id-1 ELSE id+1 END AS id
   FROM  seat
 
-数学和逻辑	
 
-【IS NULL】WHERE   referee_id IS NULL
-
-【ISNULL( )】类似于IFERROR: 
-Select ISNULL(field, 100) 
-或者
-Select isnull (result of a select, null) 
-或者
-Select isnull (result of a select, 100)
-
-【COUNT( --- )】语法： Count(column), Count (*)
-  
-【COUNT (DISTINCT  --- )】
-SELECT a, COUNT(DISTINCT b) FROM table GROUP BY a
-
-【SUM】这类的函数里面还可以有运算的，比如
-SELECT a, SUM(b-c) 
-FROM tbl 
-GROUP BY a
-
-【求余数】employee_id % 2 = 1 判断employee_id是奇偶数
-
-【AND/OR】WHERE   area>3 OR population >25
-
-【NOT】【Like】【%】name NOT LIKE 'M%', %可以代表空。	
-
-+----+-----+-----+-----+
-| Id | Num | LAG | LEAD|
-+----+-----+-----+-----+
-| 1  | 10  |NULL | 20  |
-| 2  | 20  | 10  | 30  |
-| 3  | 30  | 20  | 40  |
-| 4  | 40  | 30  | 50  |
-| 5  | 50  | 40  |NULL |
-+----+-----+-----+-----+
-【计算日期：dateadd(day,1,date )】
-SELECT dateadd(day,1, date) as new_date, value FROM tbl
-
-【日期期间】
-WHERE a.activity_date <='2019-07-27' AND a.activity_date > dateadd(day,-30,'2019-07-27')
-
-【某年DATEPART( )】
-where datepart(YYYY, 某time_stamp) = 2020
-  
